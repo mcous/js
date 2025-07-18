4e5f94d667d3f23df9bd2df20976ac43018eba26
Michael Cousins <michael@cousins.io>
2025-07-17 20:49:47 -0400

    fix(eslint-config): update dependencies

--- packages/eslint-config/src/types.d.ts
+++ packages/eslint-config/src/types.d.ts
@@ -1,11 +1,3 @@
-declare module 'eslint-config-prettier' {
-  import type { ConfigWithExtends } from 'typescript-eslint'
-
-  const config: ConfigWithExtends
-
-  export default config
-}
-
 declare module 'eslint-plugin-promise' {
   import type { ConfigWithExtends } from 'typescript-eslint'
 
